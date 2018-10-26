b57249c2636c966955a0e3649cdf1c4b96669b3f
Belen Fernandez <belen.milagros.1996@gmail.com>
2018-10-26 16:00:55 -0500

    Added github page

---
+++ clicky-game/precache-manifest.f4aa77fc834651914e7df688b29aa6a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "99c09bcd98cf274f8a01",
+    "url": "/clicky-game/static/js/main.99c09bcd.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/clicky-game/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "99c09bcd98cf274f8a01",
+    "url": "/clicky-game/static/css/main.d9665b5e.chunk.css"
+  },
+  {
+    "revision": "67dfa8741cf2ca15c19e1efdefd31414",
+    "url": "/clicky-game/index.html"
+  }
+];
